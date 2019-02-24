900a8e4a609797bd04ee176d06f670d9775dffb3
Ben Lardie <benlardie@gmail.com>
2019-02-23 23:14:51 -0500

    made a website

---
+++ main.js
@@ -0,0 +1,7 @@
+function toggle_visibility(cat-image) {
+     var e = document.getElementById("bubble");
+     if(e.style.display == 'block')
+        e.style.display = 'none';
+     else
+        e.style.display = 'block';
+}
